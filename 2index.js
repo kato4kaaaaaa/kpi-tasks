0a5256e562523c5fc387c05646b321c0ca8292ec
kato4kaaaaaa <155968656+kato4kaaaaaa@users.noreply.github.com>
2024-12-22 19:54:28 +0200

    Update 2index.js

--- 2index.js
+++ 2index.js
@@ -1,1 +1,11 @@
+  Task 2
+  * Prepare promise based alternative
+  * Write use cases for the promise based solution
+  * Write use cases for the async-await
+  * Add new on-demend feature during review
+    e.g.: Add support for parallelism
+
+  Note: for technologies that do not have the native Future-like async functionalities
+  You may combine Task 1 and 2 into a single Task that will showcase the 
+
 
