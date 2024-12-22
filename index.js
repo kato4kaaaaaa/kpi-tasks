a2a94f82dfb61a19327b71b21f763eda93606673
kato4kaaaaaa <155968656+kato4kaaaaaa@users.noreply.github.com>
2024-12-22 18:18:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+  Task 1
+  * Choose array fn (map/filter/filterMap/some/find/findIndex)
+  * Prepare its callback based async counterpart
+  * Prepare demo cases for the usage
+  * Add new on-demend feature during review
+    e.g.: Add support for debounce (if the task took less then X time to
+    complete -- add an additional execution delya)
 
