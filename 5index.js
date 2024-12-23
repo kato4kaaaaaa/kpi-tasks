90a4785ba35c70b21a1fb3249b53250c59c31ea2
kato4kaaaaaa <155968656+kato4kaaaaaa@users.noreply.github.com>
2024-12-23 13:34:01 +0200

    Update 5index.js

--- 5index.js
+++ 5index.js
@@ -1,4 +1,21 @@
   Task 5 
     (Observable/EventEmitter/Alternative) -- Reactive message based communication between entities
 
+const EventEmitter = require('events');
 
+class ReactiveMessageBus extends EventEmitter {}
+
+const messageBus = new ReactiveMessageBus();
+
+function sum(a, b) {
+  return a + b;
+}
+
+messageBus.on('syncOperation', (a, b) => {
+  try {
+    const result = sum(a, b);
+    console.log(`Результат суми: ${result}`);
+  } catch (error) {
+    console.error('Помилка синхронної операції:', error);
+  }
+});
