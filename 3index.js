d219496417183f47e12ab868528eae79d3eebaf2
kato4kaaaaaa <155968656+kato4kaaaaaa@users.noreply.github.com>
2024-12-23 12:02:46 +0200

    Update 3index.js

--- 3index.js
+++ 3index.js
@@ -1,3 +1,20 @@
   Task 3
   * Integrate AbortController or other Cancallable approach
-
+// abortableFilter.js
+function abortableFilter(array, callback, controller) {
+  if (!Array.isArray(array)) throw new Error("Не масив"); // Перевірка на масив
+  let signal = controller ? controller.signal : null;
+  let results = [];
+  let promises = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    let current = array[i];
+    promises.push(
+      new Promise(resolve => {
+        if (signal && signal.aborted) {
+          resolve(false);
+        } else {
+          setTimeout(() => {
+            try {
+              callback(current, i, array, signal).then(res => {
+                results.push(res ? current : null);
